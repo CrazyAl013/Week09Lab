72cd5e9910b410ba44ecaffe7d6e00218723da4d
Brendan Tran <btran418@gmail.com>
2020-10-27 09:42:02 -0600

    Week 6 Lab
    Updated the service java classes.
    - UserService now has CRUD operations
    - RoleService now has getAll operation

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -5,10 +5,65 @@
  */
 package services;
 
+import dataaccess.UserDB;
+
 /**
  *
  * @author BTran
  */
 public class UserService {
+    public User get(String email) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        return user;
+    }
     
+    public User get(boolean activity) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(activity);
+        return user;
+    }
+    
+    public User get(String first_name) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(first_name);
+        return user;
+    }
+    
+    public User get(String last_name) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(last_name);
+        return user;
+    }
+    
+    public User get(String password) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(password);
+        return user;
+    }
+    
+    public void insert(String email, boolean activity, String first_name, String last_name, String password, int role) throws Exception{
+        User user = new User(0, email, activity, first_name, last_name, password, role);
+        UserDB userDB = new UserDB();
+        userDB.insert(user);
+    }
+    
+    public void update(int userID, String email, boolean activity, String first_name, String last_name, String password, int role) throws Exception{
+        User user = new User(0, email, activity, first_name, last_name, password, role);
+        UserDB userDB = new UserDB();
+        userDB.insert(user);
+    }
+    
+    public void delete(int userID) throws Exception{
+        User user = new User();
+        user.setUserID(userID);
+        UserDB userDB = new UserDB();
+        userDB.delete(user);
+    }
 }
+
+
+
+
+
+
