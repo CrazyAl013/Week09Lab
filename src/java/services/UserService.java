4ef631fecdc8c8f507dd0cb44221ba37f6d28f0a
584893 <584893@SAIT224204IT.ACDM.DS.SAIT.CA>
2020-10-29 13:45:24 -0600

    Changed user services

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -25,40 +25,16 @@ public class UserService {
         return user;
     }
     
-    public User get(boolean activity) throws Exception {
-        UserDB userDB = new UserDB();
-        User user = userDB.get(activity);
-        return user;
-    }
-    
-    public User get(String first_name) throws Exception {
-        UserDB userDB = new UserDB();
-        User user = userDB.get(first_name);
-        return user;
-    }
-    
-    public User get(String last_name) throws Exception {
-        UserDB userDB = new UserDB();
-        User user = userDB.get(last_name);
-        return user;
-    }
-    
-    public User get(String password) throws Exception {
-        UserDB userDB = new UserDB();
-        User user = userDB.get(password);
-        return user;
-    }
-    
     public void insert(String email, boolean activity, String first_name, String last_name, String password, int role) throws Exception{
         User user = new User(email, activity, first_name, last_name, password, role);
         UserDB userDB = new UserDB();
         userDB.insert(user);
     }
     
-    public void update(int userID, String email, boolean activity, String first_name, String last_name, String password, int role) throws Exception{
-        User user = new User(0, email, activity, first_name, last_name, password, role);
+    public void update(String email, boolean activity, String first_name, String last_name, String password, int role) throws Exception{
+        User user = new User(email, activity, first_name, last_name, password, role);
         UserDB userDB = new UserDB();
-        userDB.insert(user);
+        userDB.update(user);
     }
     
     public void delete(int userID) throws Exception{
