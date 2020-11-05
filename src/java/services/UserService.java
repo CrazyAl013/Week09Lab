4ffe248626ab326d63386704f06a32b939322cf2
Brendan Tran <btran418@gmail.com>
2020-11-05 14:04:16 -0700

    Lab 9
    - fixed UserService insert for getting role using roleID

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -23,40 +23,25 @@ public class UserService {
         List<User> users = userDB.getAll();
         return users;
     }
+    
     public User get(String email) throws Exception {
         UserDB userDB = new UserDB();
         User user = userDB.get(email);
         return user;
     }
     
-    public void insert(String email, boolean activity, String first_name, String last_name, String password, Role id) throws Exception{
+    public void insert(String email, boolean activity, String first_name, String last_name, String password, int roleID) throws Exception{
         
         //TODO:
         // replace Role role parameter with a int id, then get a Role object from RoleDB using the role id. then assign it to the user
         // also add user to the Role User list
-        
-        User user = new User(email, activity, first_name, last_name, password, id);
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.get(roleID);
+        User user = new User(email, activity, first_name, last_name, password, role);
         UserDB userDB = new UserDB();
         userDB.insert(user);
-         RoleDB roleDB = new RoleDB();
-        
-      
-        
         
     }
-    public class RoleDB {
-    public List<Role> getAll() throws Exception {
-        EntityManager em = DBUtil.getEmFactory().createEntityManager();
-
-        try {
-            List<Role> roles = em.createNamedQuery("Role.findAll", Role.class).getResultList();
-            return roles;
-        } finally {
-            em.close();
-        }
-    }
-    
-    
     
     public void update(String email, boolean activity, String first_name, String last_name, String password, Role role) throws Exception{
         UserDB userDB = new UserDB();
@@ -71,9 +56,6 @@ public class UserService {
         UserDB userDB = new UserDB();
         User user = userDB.get(email);
         userDB.delete(user);
-    }
-
-  
     }
 }
 
